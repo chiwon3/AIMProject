3faaffd641620014e4dbd253cd8f853791384b22
YOON <chiwon3@gmail.com>
2020-09-12 16:16:19 +0900

    나가지 않는 진도, 미세한 디자인 변화, 늘지않는 나의 실력

--- mypage/urls.py
+++ mypage/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("create_page/<int:pk>", views.create1 , name="create_page"),
-    path("edit_page/<int:pk>", views.create1 , name="edit_page"),
+    path("edit_page1/<int:pk>", views.create1 , name="edit_page1"),
+    path("edit_page2/<int:pk>", views.create2 , name="edit_page2"),
+    path("edit_page3/<int:pk>", views.create3 , name="edit_page3"),
 ]
--- mypage/views.py
+++ mypage/views.py
@@ -19,35 +19,86 @@ def index(request):
     return render(request, 'index_copy.html', context)
 
 
+# def create1(request,pk):
+#     context = dict()
+    
+#     if request.method == "POST":
+#         temp_form1 = Urlform1(request.POST)
+#         temp_form2 = Urlform2(request.POST)
+#         temp_form3 = Urlform3(request.POST)
+#         if temp_form1.is_valid() or temp_form2.is_valid() or temp_form3.is_valid():
+#             success_form1 = temp_form1.save(commit=False)
+#             success_form2 = temp_form2.save(commit=False)
+#             success_form3 = temp_form3.save(commit=False)
+#             success_form1.user = User.objects.get(id = pk)
+#             success_form2.user = User.objects.get(id = pk)
+#             success_form3.user = User.objects.get(id = pk)
+#             success_form1.save()
+#             success_form2.save()
+#             success_form3.save()
+#             return redirect('index')
+#         else:
+#             context["temp_context1"] = temp_form1
+#             context["temp_context2"] = temp_form2
+#             context["temp_context3"] = temp_form3
+#             return render(request,'edit.html',context)
+#     else:
+#         context["temp_context1"] = Urlform1()
+#         context["temp_context2"] = Urlform2()
+#         context["temp_context3"] = Urlform3()
+#         return render(request,'edit.html', context)
+
 def create1(request,pk):
     context = dict()
     
     if request.method == "POST":
         temp_form1 = Urlform1(request.POST)
-        temp_form2 = Urlform2(request.POST)
-        temp_form3 = Urlform3(request.POST)
-        if temp_form1.is_valid() or temp_form2.is_valid() or temp_form3.is_valid():
+        if temp_form1.is_valid():
             success_form1 = temp_form1.save(commit=False)
-            success_form2 = temp_form2.save(commit=False)
-            success_form3 = temp_form3.save(commit=False)
             success_form1.user = User.objects.get(id = pk)
-            success_form2.user = User.objects.get(id = pk)
-            success_form3.user = User.objects.get(id = pk)
             success_form1.save()
-            success_form2.save()
-            success_form3.save()
             return redirect('index')
         else:
             context["temp_context1"] = temp_form1
-            context["temp_context2"] = temp_form2
-            context["temp_context3"] = temp_form3
             return render(request,'edit.html',context)
     else:
         context["temp_context1"] = Urlform1()
-        context["temp_context2"] = Urlform2()
-        context["temp_context3"] = Urlform3()
         return render(request,'edit.html', context)
 
+def create2(request,pk):
+    context = dict()
+    
+    if request.method == "POST":
+        temp_form2 = Urlform2(request.POST)
+        if temp_form2.is_valid():
+            success_form2 = temp_form2.save(commit=False)
+            success_form2.user = User.objects.get(id = pk)
+            success_form2.save()
+            return redirect('index')
+        else:
+            context["temp_context1"] = temp_form2
+            return render(request,'edit.html',context)
+    else:
+        context["temp_context2"] = Urlform2()
+        return render(request,'edit.html', context)
+
+
+def create3(request,pk):
+    context = dict()
+    
+    if request.method == "POST":
+        temp_form3 = Urlform3(request.POST)
+        if temp_form3.is_valid():
+            success_form3 = temp_form3.save(commit=False)
+            success_form3.user = User.objects.get(id = pk)
+            success_form3.save()
+            return redirect('index')
+        else:
+            context["temp_context3"] = temp_form3
+            return render(request,'edit.html',context)
+    else:
+        context["temp_context3"] = Urlform1()
+        return render(request,'edit.html', context)
 
 def update1(request,post_id):
     context = dict()
@@ -56,12 +107,24 @@ def update1(request,post_id):
         temp_form1 = Urlform1(request.POST,instance=CustomUrl1.objects.get(id = post_id))
         temp_form2 = Urlform2(request.POST,instance=CustomUrl2.objects.get(id = post_id))
         temp_form3 = Urlform3(request.POST,instance=CustomUrl3.objects.get(id = post_id))
-        if temp_form.is_valid():
-            temp_form.save()
+        if temp_form1.is_valid():
+            temp_form1.save()
+            return redirect('index')
+        
+        elif temp_form2.is_valid():
+            temp_form2.save()
+            return redirect('index')
+        
+        elif temp_form3.is_valid():
+            temp_form3.save()
             return redirect('index')
         else:
-            context["temp_context"] = temp_form
+            context["temp_context1"] = temp_form1
+            context["temp_context2"] = temp_form2
+            context["temp_context3"] = temp_form3
             return render(request,'edit.html',context)
     else:
-        context["temp_context"] = Urlform1(instance=CustomUrl1.objects.get(id = post_id))
+        context["temp_context1"] = Urlform1(instance=CustomUrl1.objects.get(id = post_id))
+        context["temp_context2"] = Urlform2(instance=CustomUrl1.objects.get(id = post_id))
+        context["temp_context3"] = Urlform3(instance=CustomUrl1.objects.get(id = post_id))
         return render(request,'edit.html', context)
