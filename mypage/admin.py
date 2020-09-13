5e9912abd0922b8656ac86062c4919c8c7e8aaf7
YOON <chiwon3@gmail.com>
2020-09-13 18:30:30 +0900

    게시판 바꾸려고 시도는 함, index custom 부분도 바꾸려고 시도만 함

--- mypage/admin.py
+++ mypage/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import *
+from .models import CustomUrl1,CustomUrl2,CustomUrl3
 
 
+class ProfileInline(admin.StackedInline):
+    model = CustomUrl1
+    model = CustomUrl2
+    model = CustomUrl3
 
 admin.site.register(CustomUrl1)
 admin.site.register(CustomUrl2)
